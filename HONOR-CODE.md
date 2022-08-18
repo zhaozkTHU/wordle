0ff57bc1e7644f498e489720adcc99c99dec41f4
zhaozk21 <2326060921@qq.com>
2022-08-18 16:49:09 +0800

    finish game if is_tty == true

--- HONOR-CODE.md
+++ HONOR-CODE.md
@@ -1,1 +1,4 @@
 # Honor Code
+https://wsdjeg.spacevim.org/rust-modules-and-files/
+https://stackoverflow.com/questions/47829646/how-do-i-convert-a-string-to-a-list-of-chars
+https://stackoverflow.com/questions/69868409/how-do-i-split-my-rust-program-into-many-files
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use console;
+use rand::{self, Rng};
 use std::io::{self, Write};
 
 /// The main function for the Wordle game, implement your own logic here
@@ -29,6 +30,66 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("");
     // TODO: parse the arguments in `args`
+
+    if is_tty {
+        use builtin_words::{ACCEPTABLE, FINAL};
+        let answer_word: String = FINAL[rand::thread_rng().gen_range(0..FINAL.len())].to_string();
+        println!("{}", &answer_word);
+        let mut win = false;
+        for _ in 0..6 {
+            let mut guess_words: String;
+            let mut correct = 0;
+            loop {
+                guess_words = text_io::read!();
+                if guess_words.len() < 5 {
+                    print!("{}", console::style("Not enough letters").bold().red());
+                    continue;
+                }
+                if guess_words.len() > 5 {
+                    print!("{}", console::style("Too many letters.").bold().red());
+                }
+                if !ACCEPTABLE.contains(&guess_words.as_str()) {
+                    println!("{}", console::style("Not in word list").bold().red());
+                    continue;
+                }
+                break;
+            }
+            for i in 0..guess_words.len() {
+                let answer_vec: Vec<char> = answer_word.clone().chars().collect();
+                let guess_vec: Vec<char> = guess_words.clone().chars().collect();
+                if answer_vec.contains(&guess_vec[i]) {
+                    if answer_vec[i] == guess_vec[i] {
+                        print!("{}", console::style(&guess_vec[i]).green());
+                        correct += 1;
+                    } else {
+                        print!("{}", console::style(&guess_vec[i]).yellow());
+                    }
+                } else {
+                    print!("{}", console::style(&guess_vec[i]).red());
+                }
+            }
+            print!("\n");
+            if correct == 5 {
+                println!(
+                    "{}",
+                    console::style("Congratulations, you find the answer!")
+                        .green()
+                        .bold()
+                );
+                win = true;
+                break;
+            }
+        }
+        if win == false {
+            println!(
+                "{}",
+                console::style("Sorry, you lose the game.Please try again.")
+                    .red()
+                    .bold()
+            )
+        }
+    } else {
+    }
 
     Ok(())
 }
